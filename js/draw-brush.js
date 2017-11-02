88a8827481d442ca259986199b0f8bbd73a66640
Justin Ho <justintingfung@gmail.com>
2017-11-02 23:48:12 +0800

    Filled spaces between dots, removed globalalpha inheritance, added save function

--- js/draw-brush.js
+++ js/draw-brush.js
@@ -31,39 +31,49 @@ class DrawBrush extends PaintFunction {
         this.contextReal.strokeStyle = this.colorStroke;
         this.contextReal.lineJoin = 'round';
         this.contextReal.lineCap = 'round'
+        this.isDrawing = true;
     }
 
     onDragging(coord, event) {
+        if (!this.isDrawing) return;
+
         this.contextReal.beginPath();
         this.contextReal.globalAlpha = 1;        
-        this.contextReal.moveTo(coord[0] - 4, coord[1] - 4);
+        this.contextReal.moveTo(this.origX - 4, this.origY - 4);
         this.contextReal.lineTo(coord[0] - 4, coord[1] - 4);
         this.contextReal.stroke();
 
         this.contextReal.globalAlpha = 0.6;
-        this.contextReal.moveTo(coord[0] - 2, coord[1] - 2);
+        this.contextReal.moveTo(this.origX - 2, this.origY - 2);
         this.contextReal.lineTo(coord[0] - 2, coord[1] - 2);
         this.contextReal.stroke();
 
         this.contextReal.globalAlpha = 0.4;
-        this.contextReal.moveTo(coord[0], coord[1]);
+        this.contextReal.moveTo(this.origX, this.origY);
         this.contextReal.lineTo(coord[0], coord[1]);
         this.contextReal.stroke();
 
         this.contextReal.globalAlpha = 0.3;
-        this.contextReal.moveTo(coord[0] + 2, coord[1] + 2);
+        this.contextReal.moveTo(this.origX + 2, this.origY + 2);
         this.contextReal.lineTo(coord[0] + 2, coord[1] + 2);
         this.contextReal.stroke();
 
         this.contextReal.globalAlpha = 0.2;
-        this.contextReal.moveTo(coord[0] + 4, coord[1] + 4);
+        this.contextReal.moveTo(this.origX + 4, this.origY + 4);
         this.contextReal.lineTo(coord[0] + 4, coord[1] + 4);
         this.contextReal.stroke();
 
+        this.origX = coord[0];
+        this.origY = coord[1];
     }
 
     onMouseMove() { }
-    onMouseUp() { }
+    onMouseUp() {
+        this.contextReal.globalAlpha = 1;
+        this.isDrawing = false;
+        //Add the following code when you draw on canvas real for undo
+        saveCanvas();
+    }
     onMouseLeave() { }
     onMouseEnter() { }
 }
